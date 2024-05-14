5f37ffa835f42efd0dfd2c8babab4aadcfb2d838
Andrey Zuev <stepgt@mail.ru>
2024-05-14 10:33:27 +1000

    Config app module

--- server/costs/src/app.module.ts
+++ server/costs/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './config/MongooseConfigService';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: MongooseConfigService,
+    }),
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
 })
 export class AppModule {}
