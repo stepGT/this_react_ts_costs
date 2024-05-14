a4da24178b29513022f64a88a3c8cdea02016a7a
Andrey Zuev <stepgt@mail.ru>
2024-05-14 16:07:41 +1000

    Added UserSchema

--- server/costs/src/app.module.ts
+++ server/costs/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './config/MongooseConfigService';
 import configuration from './config/configuration';
+import { UserModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import configuration from './config/configuration';
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    UserModule,
   ],
 })
 export class AppModule {}
