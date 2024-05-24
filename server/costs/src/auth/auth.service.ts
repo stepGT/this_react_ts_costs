56656f863c6ee7441ae39f17c9453609ca02cd5d
Andrey Zuev <stepgt@mail.ru>
2024-05-24 16:44:47 +1000

    Added CostsController & CostsModule

--- server/costs/src/auth/auth.service.ts
+++ server/costs/src/auth/auth.service.ts
@@ -34,4 +34,33 @@ export class AuthService {
       ),
     };
   }
+
+  verifyToken(token: string) {
+    try {
+      return this.jwtService.verify(token);
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+
+  parseJwt(token: string) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join(''),
+    );
+
+    return JSON.parse(jsonPayload);
+  }
+
+  async getUserByTokenData(token: string): Promise<User> {
+    const parsedTokenData = this.parseJwt(token);
+
+    return await this.usersService.findOne(parsedTokenData.user.username);
+  }
 }
---
+++ server/costs/src/costs/costs.controller.ts
@@ -0,0 +1,42 @@
+﻿import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+import { JWTGuard } from '../auth/guards/jwt.guard';
+import { CostsService } from './costs.service';
+import { CreateCostDto } from './dto/create-cost.dto';
+import { UpdateCostDto } from './dto/update-cost.dto';
+
+@Controller('cost')
+export class CostsController {
+  constructor(
+    private readonly costsService: CostsService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @UseGuards(JWTGuard)
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getAllCosts(@Req() req, @Res() res) {
+    const token = req.token;
+
+    const user = await this.authService.getUserByTokenData(token);
+    const costs = await this.costsService.findAll();
+    const filteredCosts = costs.filter(
+      (cost) => cost.userId === user._id.toString(),
+    );
+
+    return res.send(filteredCosts);
+  }
+}
