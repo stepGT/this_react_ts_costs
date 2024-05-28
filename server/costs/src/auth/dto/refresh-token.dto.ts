b8cc1b75d27bd53358f8458f860e2c7f8cf5777a
Andrey Zuev <stepgt@mail.ru>
2024-05-28 16:18:51 +1000

    Added refreshToken

---
+++ server/costs/src/auth/dto/refresh-token.dto.ts
@@ -0,0 +1,9 @@
+﻿import { IsNotEmpty } from 'class-validator';
+
+export class RefreshTokenDto {
+  @IsNotEmpty()
+  readonly refresh_token: string;
+
+  @IsNotEmpty()
+  readonly username: string;
+}
