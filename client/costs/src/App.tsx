17d7819bd92aab74dc3739b710fae694586a4a30
Andrey Zuev <stepgt@mail.ru>
2024-05-31 15:42:56 +1000

    Added component Header

--- client/costs/src/App.tsx
+++ client/costs/src/App.tsx
@@ -1,5 +1,11 @@
+import { Header } from './components/Header';
+
 const App = () => {
-  return <div className="App">Costs</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
