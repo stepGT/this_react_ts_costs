774ad6111246b5f1141d2d8ee072a30fb6157fd5
Andrey Zuev <stepgt@mail.ru>
2024-06-03 16:22:54 +1000

    Added router

--- client/costs/src/App.tsx
+++ client/costs/src/App.tsx
@@ -1,9 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { AuthPage } from './components/AuthPage/AuthPage';
 import { Header } from './components/Header';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthPage type="login" />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
