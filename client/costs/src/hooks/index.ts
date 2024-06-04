3de0c35045bc3550a8e12624459d7fa6faa994b1
Andrey Zuev <stepgt@mail.ru>
2024-06-04 11:41:01 +1000

    Changed dark theme link

--- client/costs/src/hooks/index.ts
+++ client/costs/src/hooks/index.ts
@@ -2,7 +2,7 @@
 
 export const useTheme = () => {
   const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme') as string) || 'dark');
-  const darkTheme = 'https://cdn.jsdelivr.net/npm/@forevolve/bootstrap-dark@1.0.0/dist/css/bootstrap-dark.min.css';
+  const darkTheme = 'https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-dark.min.css';
   const lightTheme = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
 
   const setCurrentMode = (theme: string) => {
